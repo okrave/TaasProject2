c297b34dfcd86f6d176631db52264fd9c3012133
okrave <lomanto.calogero_luca_maria@educ.di.unito.it>
2019-05-31 11:10:51 +0200

    nessun cambiamento

--- src/main/java/com/example/togroup5/demo/controllers/RestGroupController.java
+++ src/main/java/com/example/togroup5/demo/controllers/RestGroupController.java
@@ -3,7 +3,6 @@ package com.example.togroup5.demo.controllers;
 import com.example.togroup5.demo.entities.AppGroup;
 import com.example.togroup5.demo.servicies.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +19,4 @@ public class RestGroupController {
         List<AppGroup> allGroup = groupService.listAllGroup();
         return allGroup;
     }
-
 }
