624c6cb6ae308949795275402b438d964cd00dc0
acptIA <acpt1994@gmail.com>
2019-06-02 23:57:30 +0200

    A bean with that name has already been defined in ...

--- src/main/java/com/example/togroup5/demo/DemoApplication.java
+++ src/main/java/com/example/togroup5/demo/DemoApplication.java
@@ -2,9 +2,41 @@ package com.example.togroup5.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableOAuth2Sso
+@RestController
+public class DemoApplication extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @RequestMapping("/user")
+    public Principal user (Principal principal) {
+        return principal;
+    }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
