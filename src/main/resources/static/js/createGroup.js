dbfaa6e8892fb77ad483cfd7e5816b3db3ccce1b
lonevetad <ottins1995@gmail.com>
2019-06-07 12:27:25 +0200

    Start integrating location search on new-group using Google Maps Api (Place, Geolocalization, etc)

--- src/main/resources/static/js/createGroup.js
+++ src/main/resources/static/js/createGroup.js
@@ -15,22 +15,50 @@ class NewGroupProgression{
         this.maxSteps = 4;
     }
 
-    setStep(){
-
+    toNextStep(){
+        if(this.actualStep < this.maxSteps){
+            this.actualStep = this.actualStep + 1;
+        }
+    }
+    toNextStep(){
+        if(this.actualStep > 0){
+            this.actualStep = this.actualStep - 1;
+        }
     }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-window.onload = _ => {
+const googleMapApis = "https://maps.googleapis.com/maps/api/place/autocomplete/json?";
+
+
+//window.onload =
+function initAll() {
     app = new Vue({
         el: "#appNewGroup",
         data: {
             toGroupAPI: new ToGroup()
             , messages: new NotificationsMessage()
             , tag: ""
+            , locationSearch: ""
             , newGroupInfo: new GroupNew()
+            , creationProgression : new NewGroupProgression()
             , allTags: [] //yet existing tags on database
+
+            //google maps
+            , gMapData: {
+                map			: null,
+                mapOptions	: {
+                    zoom: 8,
+                    center: null
+                },
+                defaultBounds : null,
+                optionsAutocomplete: null,
+                autocomplete: null
+            }
+        },
+        mounted(){
+            this.setUpGoogleMapsStuffs();
         },
         created() {
             let thisVue = this;
@@ -39,6 +67,33 @@ window.onload = _ => {
         },
 
         methods: {
+            setUpGoogleMapsStuffs(){
+                var inputElement, thisVue;
+                thisVue = this;
+                inputElement = document.getElementById("locationQuery");
+                console.log("setting up google maps api START");
+
+                thisVue.gMapData.mapOptions.center = new google.maps.LatLng(45.075741, 7.673600);
+                thisVue.gMapData.map = new google.maps.Map(
+                    document.getElementById('map-canvas'),
+                    thisVue.gMapData.mapOptions
+                );
+                thisVue.gMapData.defaultBounds = new google.maps.LatLngBounds(
+                    new google.maps.LatLng(45.07, 7.67),
+                    new google.maps.LatLng(45.08, 7.68)
+                );
+                thisVue.gMapData.optionsAutocomplete = {bounds: thisVue.gMapData.defaultBounds};
+
+                thisVue.gMapData.map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputElement);
+                thisVue.gMapData.autocomplete = new google.maps.places.Autocomplete(
+                    inputElement,
+                    thisVue.gMapData.optionsAutocomplete
+                );
+
+                console.log("setting up google maps api END");
+            },
+
+
             createErrorHandler(methodName) {
                 let thisVue = this;
                 return function (err) {
@@ -95,6 +150,16 @@ window.onload = _ => {
                     })
                     .catch(this.createErrorHandler("list all tags"));
             }
+
+            , getLocationFromAddress(){
+                console.log("locationSearch: "+locationSearch);
+                // https://maps.googleapis.com/maps/api/place/js/AutocompletionService.GetPredictions?1sTorino polo del
+                // &4sit-IT&6m6&1m2&1d44.96502155524857&2d7.43074650610356&2m2&1d45.18613135982509&2d7.917921493896529&14e3&15e3&20sCC2E2C53-0C2D-4E4F-9F4B-CCD975A78F2964w8tengi17z&21m1&2e1&callback=_xdc_._uwrn9u&client=gme-addictive&channel=geocoder-tool&token=44656
+                //googleGeolocalisation
+            }
         }
     });
-};
+}
+
+
+google.maps.event.addDomListener(window, 'load', initAll);
